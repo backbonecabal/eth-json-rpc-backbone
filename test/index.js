4d4f38919c8e58fddef5afe515ecd406d21a4cac
sam bacha <sam@freighttrust.com>
2021-01-11 00:02:34 -0800

    chore(paths): update testing for backbone

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 const test = require('tape')
-const createInfuraMiddleware = require('../src')
+const createBackboneMiddleware = require('../src')
 
-const { fetchConfigFromReq } = createInfuraMiddleware
+const { fetchConfigFromReq } = createBackboneMiddleware
 
 test('fetchConfigFromReq - basic', (t) => {
 
@@ -14,14 +14,14 @@ test('fetchConfigFromReq - basic', (t) => {
     params: ['0x482103', true],
   }
 
-  const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req, source: 'eth-json-rpc-infura', projectId })
-  t.equals(fetchUrl, 'https://mainnet.infura.io/v3/foo')
+  const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req, source: 'eth-json-rpc-backbone', projectId })
+  t.equals(fetchUrl, 'https://mainnet.backbonecabal.com/v1/foo')
   t.deepEquals(fetchParams, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      'Infura-Source': 'eth-json-rpc-infura/internal',
+      'Manifold-Source': 'eth-json-rpc-backbone/internal',
     },
     body: '{"id":1,"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["0x482103",true]}',
   })
@@ -41,7 +41,7 @@ test('fetchConfigFromReq - basic: no source specified', (t) => {
   }
 
   const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req, projectId })
-  t.equals(fetchUrl, 'https://mainnet.infura.io/v3/foo')
+  t.equals(fetchUrl, 'https://mmainnet.backbonecabal.com/v1/foo')
   t.deepEquals(fetchParams, {
     method: 'POST',
     headers: {
@@ -56,7 +56,7 @@ test('fetchConfigFromReq - basic: no source specified', (t) => {
 
 test('fetchConfigFromReq - basic', (t) => {
 
-  const network = 'ropsten'
+  const network = 'mainnet'
   const projectId = 'foo'
   const req = {
     jsonrpc: '2.0',
@@ -65,14 +65,14 @@ test('fetchConfigFromReq - basic', (t) => {
     params: ['0x0102030405060708090a0b0c0d0e0f'],
   }
 
-  const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req, source: 'eth-json-rpc-infura', projectId })
-  t.equals(fetchUrl, 'https://ropsten.infura.io/v3/foo')
+  const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req, source: 'eth-json-rpc-backbone', projectId })
+  t.equals(fetchUrl, 'https://mainnet.backbonecabal.com/v1/foo')
   t.deepEquals(fetchParams, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      'Infura-Source': 'eth-json-rpc-infura/internal',
+      'Manifold-Source': 'eth-json-rpc-backbone/internal',
     },
     body: '{"id":1,"jsonrpc":"2.0","method":"eth_sendRawTransaction","params":["0x0102030405060708090a0b0c0d0e0f"]}',
   })
@@ -82,16 +82,16 @@ test('fetchConfigFromReq - basic', (t) => {
 
 test('fetchConfigFromReq - strip non-standard keys', (t) => {
 
-  const network = 'ropsten'
+  const network = 'mainnet'
   const projectId = 'foo'
   const req = {
     method: 'eth_sendRawTransaction',
     params: ['0x0102030405060708090a0b0c0d0e0f'],
-    origin: 'happydapp.eth',
+    origin: 'backbonecabal.eth',
   }
 
   const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req, projectId })
-  t.equals(fetchUrl, 'https://ropsten.infura.io/v3/foo')
+  t.equals(fetchUrl, 'https://mainnet.backbonecabal.com/v3/foo')
   const parsedReq = JSON.parse(fetchParams.body)
   t.notOk('origin' in parsedReq, 'non-standard key removed from req')
   t.end()
@@ -100,24 +100,24 @@ test('fetchConfigFromReq - strip non-standard keys', (t) => {
 
 test('fetchConfigFromReq - source specified for request origin in header', (t) => {
 
-  const network = 'ropsten'
+  const network = 'mainnet'
   const projectId = 'foo'
   const req = {
     jsonrpc: '2.0',
     id: 1,
     method: 'eth_sendRawTransaction',
     params: ['0x0102030405060708090a0b0c0d0e0f'],
-    origin: 'happydapp.eth',
+    origin: 'backbonecabal.eth',
   }
 
-  const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req, source: 'eth-json-rpc-infura', projectId })
-  t.equals(fetchUrl, 'https://ropsten.infura.io/v3/foo')
+  const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req, source: 'eth-json-rpc-backbone', projectId })
+  t.equals(fetchUrl, 'https://mainnet.backbonecabal.com/v3/foo')
   t.deepEquals(fetchParams, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      'Infura-Source': 'eth-json-rpc-infura/happydapp.eth',
+      'Manifold-Source': 'eth-json-rpc-backbone/backbonecabal.eth',
     },
     body: fetchParams.body,
   })
@@ -127,7 +127,7 @@ test('fetchConfigFromReq - source specified for request origin in header', (t) =
 
 test('fetchConfigFromReq - extraHeaders specified', (t) => {
 
-  const network = 'ropsten'
+  const network = 'mainnet'
   const projectId = 'foo'
   const extraHeaders = { 'User-Agent': 'app/1.0' }
   const req = {
@@ -135,11 +135,11 @@ test('fetchConfigFromReq - extraHeaders specified', (t) => {
     id: 1,
     method: 'eth_sendRawTransaction',
     params: ['0x0102030405060708090a0b0c0d0e0f'],
-    origin: 'happydapp.eth',
+    origin: 'backbonecabal.eth',
   }
 
   const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req, projectId, extraHeaders })
-  t.equals(fetchUrl, 'https://ropsten.infura.io/v3/foo')
+  t.equals(fetchUrl, 'https://mainnet.backbonecabal.com/v3/foo')
   t.deepEquals(fetchParams, {
     method: 'POST',
     headers: {
@@ -153,21 +153,21 @@ test('fetchConfigFromReq - extraHeaders specified', (t) => {
 
 })
 
-test('createInfuraMiddleware throws with an invalid projectId', (t) => {
+test('createBackboneMiddleware throws with an invalid projectId', (t) => {
 
-  t.throws(() => createInfuraMiddleware({ projectId: 42 }), /Invalid value for 'projectId'/u)
-  t.throws(() => createInfuraMiddleware({ projectId: null }), /Invalid value for 'projectId'/u)
-  t.throws(() => createInfuraMiddleware({ projectId: undefined }), /Invalid value for 'projectId'/u)
-  t.throws(() => createInfuraMiddleware({ projectId: '' }), /Invalid value for 'projectId'/u)
+  t.throws(() => createBackboneMiddleware({ projectId: 42 }), /Invalid value for 'projectId'/u)
+  t.throws(() => createBackboneMiddleware({ projectId: null }), /Invalid value for 'projectId'/u)
+  t.throws(() => createBackboneMiddleware({ projectId: undefined }), /Invalid value for 'projectId'/u)
+  t.throws(() => createBackboneMiddleware({ projectId: '' }), /Invalid value for 'projectId'/u)
   t.end()
 
 })
 
-test('createInfuraMiddleware throws with invalid headers', (t) => {
+test('createBackboneMiddleware throws with invalid headers', (t) => {
 
-  t.throws(() => createInfuraMiddleware({ projectId: 'foo', headers: null }), /Invalid value for 'headers'/u)
-  t.throws(() => createInfuraMiddleware({ projectId: 'foo', headers: 42 }), /Invalid value for 'headers'/u)
-  t.throws(() => createInfuraMiddleware({ projectId: 'foo', headers: '' }), /Invalid value for 'headers'/u)
+  t.throws(() => createBackboneMiddleware({ projectId: 'foo', headers: null }), /Invalid value for 'headers'/u)
+  t.throws(() => createBackboneMiddleware({ projectId: 'foo', headers: 42 }), /Invalid value for 'headers'/u)
+  t.throws(() => createBackboneMiddleware({ projectId: 'foo', headers: '' }), /Invalid value for 'headers'/u)
   t.end()
 
 })
