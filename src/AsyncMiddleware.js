c4b57885cc272508d62c519729a7b084e0cc0b68
sam bacha <sam@freighttrust.com>
2021-01-10 23:52:25 -0800

    temp for hardcoded env values

---
+++ src/AsyncMiddleware.js
@@ -0,0 +1,14 @@
+// tmp file - hardcode values here 
+
+ return createAsyncMiddleware(async (req, res, _next) => {
+    if (!BACKBONE_PROJECT_ID) {
+      await setProjectId()
+    }
+
+    if (!BACKBONE_KEY) {
+      await setBraveKey()
+    }
+
+
+let BACKBONE_KEY = ''
+let BACKBONE_PROJECT_ID = ''
