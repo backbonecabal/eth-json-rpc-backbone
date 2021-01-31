add0ea9a1bd023904c67dccd803127854af20d1b
sam bacha <sam@freighttrust.com>
2021-01-31 01:36:30 -0800

    fix(provider): update cabal middleware

--- src/createProvider.js
+++ src/createProvider.js
@@ -1,11 +1,17 @@
+/**
+ * Cabal Middleware Provider
+ * @param {createCabalMiddleware}
+ * @extends providerFromEngine
+ */
+
 const RpcEngine = require("json-rpc-engine");
 const providerFromEngine = require("eth-json-rpc-middleware/providerFromEngine");
-const createInfuraMiddleware = require(".");
+const createCabalMiddleware = require(".");
 
 module.exports = createProvider;
 
 function createProvider(opts) {
   const engine = new RpcEngine();
-  engine.push(createInfuraMiddleware(opts));
+  engine.push(createCabalMiddleware(opts));
   return providerFromEngine(engine);
 }
